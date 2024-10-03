7a385a5626bf0464cb87015fe38d8c5846aab37e
william <lflower1206@gmail.com>
2024-10-03 17:24:15 +0800

    feat: login as guest

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,30 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 const App = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const login = useCallback(() => {
+    window.electronAPI.send('ws:login', 'guest\r');
+  }, []);
+
   useEffect(() => {
+    window.electronAPI.send('ws:init', '');
     window.electronAPI.on('ws:open', (data: string) => {
       console.log('ws:open', data);
     });
 
     window.electronAPI.on('ws:message', (data: string) => {
       console.log(data.replace(/(\[\d\dm)/g, '\x1b$1'));
+      ref.current!.innerHTML = data.replace(/(\[\d\dm)/g, '\x1b$1');
     });
   }, []);
 
-  return <div>test</div>;
+  return (
+    <div>
+      <div ref={ref} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
 };
 
 export default App;
